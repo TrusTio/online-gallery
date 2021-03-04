7b9fb33db99c4b70a93d22c05a230c5fdcbdc446
TrusTio <erkan.myumyunov@gmail.com>
2021-03-04 14:25:15 +0200

    Uploading to gallery possible.

---
+++ src/components/api/gallery/image.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const uploadImage = async ({ file, galleryId }) => {
+  console.log("passed info");
+  console.log(file);
+  console.log(galleryId);
+  const formData = new FormData();
+  formData.append(
+      "imageFile",
+      file,
+      file.name
+  )
+  formData.append("galleryId", galleryId);
+  const res = await axios.post("/api/v1/image", formData);
+  return res.data;
+};
--- src/pages/GalleryContents.js
+++ src/pages/GalleryContents.js
@@ -1,13 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import { useAuth } from "contexts/AuthContext";
-import { getGalleryImages } from "components/api/gallery/users";
+import { getGalleryImages, uploadImage } from "components/api/gallery";
 import { GalleryImage } from "components/generic/GalleryImage";
 
 export const GalleryContents = () => {
   const location = useLocation();
   const [galleryContents, setGalleryContents] = React.useState(null);
   const { user } = useAuth();
+  const inputFile = useRef(null);
+
+  const onButtonClick = () => {
+    inputFile.current.click();
+  };
+  const onChangeFile = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    var file = event.target.files[0];
+    const galleryId = location.state.id;
+
+    uploadFile(file, galleryId);
+  };
+
+  const uploadFile = async (file, galleryId) => {
+    const response = await uploadImage({ file, galleryId });
+    console.log(response);
+    getGalleryContents();
+  };
 
   const getGalleryContents = async () => {
     const response = await getGalleryImages(user?.id, location.state.id);
@@ -19,10 +39,25 @@ export const GalleryContents = () => {
   }, []);
 
   if (galleryContents === null) {
-    return <div>No gallery contents present.</div>;
+    return <div></div>;
   } else {
-    return galleryContents.map((image) => {
-      return <GalleryImage key={image?.id} image={image}></GalleryImage>;
-    });
+    return (
+      <div>
+        <input
+          type="file"
+          id="file"
+          ref={inputFile}
+          style={{ display: "none" }}
+          onChange={onChangeFile.bind(this)}
+        />
+        <button onClick={onButtonClick}>Upload</button>
+        {galleryContents?.length === 0 ? (
+          <div> No gallery contents present</div>
+        ) : null}
+        {galleryContents.map((image) => {
+          return <GalleryImage key={image?.id} image={image}></GalleryImage>;
+        })}
+      </div>
+    );
   }
 };
