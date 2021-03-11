d7fdc7b5b507ded8d5cc6d0a72c4782924f2b400
TrusTio <erkan.myumyunov@gmail.com>
2021-03-11 11:49:20 +0200

    Can upload images by dropping them on top of the other images in the folder

--- src/pages/GalleryContents.js
+++ src/pages/GalleryContents.js
@@ -1,15 +1,32 @@
-import React, { useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import { useAuth } from "contexts/AuthContext";
 import { getGalleryImages, uploadImage } from "components/api/gallery";
 import { GalleryImage } from "components/generic/GalleryImage";
 import { ActionBar } from "components/generic/styled";
+import { useDropzone } from "react-dropzone";
+import styled from "styled-components";
 
 export const GalleryContents = () => {
   const location = useLocation();
   const [galleryContents, setGalleryContents] = React.useState(null);
   const { user } = useAuth();
   const inputFile = useRef(null);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    const galleryId = location.state.id;
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = () => {
+        uploadFile(file, galleryId);
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   const onButtonClick = () => {
     inputFile.current.click();
@@ -34,7 +51,6 @@ export const GalleryContents = () => {
   const getGalleryContents = async () => {
     const response = await getGalleryImages(user?.id, location.state.id);
     setGalleryContents(response);
-    console.log(response);
   };
   useEffect(() => {
     getGalleryContents();
@@ -56,17 +72,22 @@ export const GalleryContents = () => {
         <ActionBar>
           <button onClick={onButtonClick}>Upload</button>
         </ActionBar>
+        <Container {...getRootProps({ refKey: "innerRef" })}>
+          <div {...getInputProps()} />
+          {galleryContents?.length === 0 ? (
+            <div> No gallery contents present</div>
+          ) : null}
 
-        {galleryContents?.length === 0 ? (
-          <div> No gallery contents present</div>
-        ) : null}
-
-        <div>
-          {galleryContents.map((image) => {
-            return <GalleryImage key={image?.id} image={image}></GalleryImage>;
-          })}
-        </div>
+          <div>
+            {galleryContents.map((image) => {
+              return (
+                <GalleryImage key={image?.id} image={image}></GalleryImage>
+              );
+            })}
+          </div>
+        </Container>
       </div>
     );
   }
 };
+const Container = styled.div``;
