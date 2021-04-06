7132494e9626702c26952f8d1804f67d2cb329f5
TrusTio <erkan.myumyunov@gmail.com>
2021-04-06 11:53:26 +0300

    Changed the DarkTheme button to a toggle

--- src/components/generic/DarkThemeToggle.js
+++ src/components/generic/DarkThemeToggle.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { func, string } from "prop-types";
+import Switch from "react-switch";
+import { ReactComponent as MoonIcon } from "assets/images/moon.svg";
+import { ReactComponent as SunIcon } from "assets/images/sun.svg";
 
 export const DarkThemeToggle = ({ theme, toggleTheme }) => {
-  return <button onClick={toggleTheme}> Dark mode</button>;
+  return (
+    <Switch
+      onChange={toggleTheme}
+      checked={theme === "dark" ? true : false}
+      uncheckedIcon={<MoonIcon style={{ fill: "red" }} />}
+      checkedIcon={<SunIcon fill="red" />}
+    ></Switch>
+  );
 };
 
 DarkThemeToggle.propTypes = {
