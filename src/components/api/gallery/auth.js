9db14b1ab1c43336c3b0fc806471c00537d0ddec
TrusTio <erkan.myumyunov@gmail.com>
2021-06-22 12:02:52 +0300

    Heroku backend signup api test

--- src/components/api/gallery/auth.js
+++ src/components/api/gallery/auth.js
@@ -2,7 +2,7 @@ import axios from "config/axios";
 
 export const signUp = async ({ username, email, password }) => {
   const res = await axios.post(
-    "https://online-gallery-react.herokuapp.com/api/v1/users/signup",
+    "https://gallery-rest-api.herokuapp.com/api/v1/users/signup",
     {
       username,
       email,
